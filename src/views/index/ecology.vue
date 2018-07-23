c744052479c6fd6f58ad2d0b290413431c9629d7
MAO-MAX <296320003@qq.com>
2018-07-23 11:05:26 +0800

    init

---
+++ src/views/index/ecology.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+	    <div class="test"></div>
+	</div>
+</template>
+
+<script>
+  export default {
+      components: {},
+      data: function () {
+          return {}
+      },
+      computed: {},
+      created() {
+      },
+      methods: {}
+  }
+</script>
+
+<style lang="less" scoped>
+  .test {
+    width: 374px;
+    height: 200px;
+    background-color: deepskyblue;
+    border: 1px saddlebrown solid;
+  }
+</style>
